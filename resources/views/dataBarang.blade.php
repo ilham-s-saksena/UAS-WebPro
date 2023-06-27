630cab63b43e9cdd304e23be22f6c3134e0e7d5a
nguhahaha <ilhamsidikc@gmail.com>
2023-06-27 11:01:01 +0700

    Fiks Selesai, nguhahahahaaa EZ

--- resources/views/dataBarang.blade.php
+++ resources/views/dataBarang.blade.php
@@ -10,16 +10,32 @@
 <body>
 
 
+
+
+
 <!-- Start block -->
-<section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased h-screen">
+<section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased h-screen relative">
+
     
+<div class="py-4 px-4 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-6">
+        <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">IndoAlfa akan segera Hadir!</span> 
+            <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </a>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Data Barang IndoAlfa Group</h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Data barag dari IndoAlfa akan diberikan bagikan secara terbuka untuk semua Karyawan Indomaret dan Alfamart agar terjadinya hubungan yang sehat dan Transparan</p>
+        
+         
+    </div>
+
+
             <div class="mx-auto max-w-screen-2xl px-4 lg:px-12">
                 <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
                     <div class="flex flex-col md:flex-row md:items-center md:justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <div class="flex-1 flex items-center space-x-2">
                             <h5>
-                                <span class="text-gray-500">All Products:</span>
-                                <span class="dark:text-white">123456</span>
+                                <span class="text-gray-500">Jumlah Data Barang</span>
+                                <span class="dark:text-white">{{ count($data) }}</span>
                             </h5>
                             
                             <button type="button" class="group" data-tooltip-target="results-tooltip">
@@ -29,7 +45,7 @@
                                 <span class="sr-only">More info</span>
                             </button>
                             <div id="results-tooltip" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                Showing 1-100 of 436 results
+                                Menampilkan {{ count($data) }} data
                                 <div class="tooltip-arrow" data-popper-arrow=""></div>
                             </div>
                         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,16 +9,16 @@
 </head>
 <body>
     
-<section class="bg-white ">
+<section class="bg-gray-50 dark:bg-gray-900 antialiased h-screen">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-screen">
         <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">IndoAlfa Group</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">Adalah Sebuah Perusahaan dimana kami bekerja sama dan membuat persaingan pasar menjadi sedikit dan berkesempatan menjadi penguasa pasar! Nguhahahahahahahaaa.....</p>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-gray-50">IndoAlfa Group</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg lg:text-xl ">Adalah Sebuah Perusahaan dimana kami bekerja sama dan membuat persaingan pasar menjadi sedikit dan berkesempatan menjadi penguasa pasar! Nguhahahahahahahaaa.....</p>
             <a href="{{ url('dataBarang') }}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 ">
                 Lihat Data Barang
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
-            <a href="{{ url('dataBarang') }}" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+            <a href="{{ url('dataBarang') }}" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-gray-100 dark:hover:text-gray-900">
                 Dashboard
             </a> 
         </div>
